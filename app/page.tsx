fa7d11213781a10588332e8c32ebb4967f71a6be
Nicolas <nicolas_312@hotmail.fr>
2025-01-21 12:27:43 +0100

    convert to high priority image and client side page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+"use client"
 import Avatar from "../components/Avatar";
 import "../styles/App.css";
+
+
+function openMainPage() {
+  console.log("Opening Main Page");    
+}
 
 export default function Home() {
   return (
@@ -9,7 +15,7 @@ export default function Home() {
         <button className="SettingsButton"></button>
       </span>
       <Avatar />
-      <button className="PlayButton">PLAY</button>
+      <button className="PlayButton" onClick={openMainPage}>PLAY</button>
     </main>
   );
 }
--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -8,6 +8,7 @@ export default function Avatar() {
             alt="Image replacing Avatar"
             width={100}
             height={100}
+            priority={true}
             />
     );
 }
