4e174b851f0fc706a0cd584b7458000d0eb3d505
Nicolas <nicolas_312@hotmail.fr>
2025-01-21 14:01:42 +0100

    preparing mainPage

--- app/mainPage/page.tsx
+++ app/mainPage/page.tsx
@@ -1,3 +1,6 @@
+import Avatar from "@/components/Avatar";
+import "@/styles/mainPage.css";
+
 export default function MainPage() {
     return (
         <main>
@@ -5,6 +8,15 @@ export default function MainPage() {
                 <h1 className="MainTitle">PORTFOLIO</h1>
                 <button className="SettingsButton"></button>
             </span>
+            <div id="mainPageContainer">
+                <Avatar />
+                <p id="description"></p>
+                <ul id="mainPageList">
+                    <li className="listSkillItem">List of Dev Skills</li>
+                    <li className="listSkillItem">Contact</li>
+                    <li className="listSkillItem">Quit Game</li>
+                </ul>
+            </div>
         </main>
     );
 }
