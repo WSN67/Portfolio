1f30fa0acecce2942b6ad4f751ed2da248551b34
Nicolas <nicolas_312@hotmail.fr>
2025-01-21 11:50:07 +0100

    fix src path for img

--- Components/Avatar.tsx
+++ Components/Avatar.tsx
@@ -2,7 +2,7 @@ export default function Avatar() {
     return (
         <img
             className="AvatarContainerSubstitute"
-            src="programming-dark.svg"
+            src="../public/programming-dark.svg"
         ></img>
     );
 }
