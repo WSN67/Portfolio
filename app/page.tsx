c05ed149eaf52b5c6b0864472bd0611e9f66fd25
Nicolas <nicolas_312@hotmail.fr>
2025-01-23 18:43:40 +0100

    adding query on URL to change layout ( still TODO )

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Avatar from "../components/Avatar";
 import "../styles/App.css";
 import Link from "next/link";
 import LayoutChoice from "../components/LayoutChoice";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
@@ -11,7 +11,40 @@ export default function Home() {
   const [desktopLayout,setDesktopLayout] = useState(false);
   const [mobileLayout,setMobileLayout] = useState(false);
   const [layoutChoiceMade,setLayoutChoiceMade] = useState(false);
-    
+  const [isLoaded,setIsLoaded] = useState(false);
+  let intervalID: NodeJS.Timeout;
+
+
+  useEffect(() => {
+    setIsLoaded(true);
+  },[]);
+
+
+  // clears interval when layout choice is made
+  useEffect(() => { 
+    return () => {
+      clearInterval(intervalID);
+    }
+  },[layoutChoiceMade]);
+
+  // clears interval if user refreshes page and 
+  // create a new one to check if a layout has been chosen
+  useEffect(() => {
+    if (intervalID) {
+      clearInterval(intervalID);      
+    }
+
+    intervalID = setInterval(() => {
+      if(window.location.search.includes("Layout=Desktop")){
+        setDesktopLayout(true);
+        setLayoutChoiceMade(true);
+      } else if(window.location.search.includes("Layout=Mobile")){
+        setMobileLayout(true);
+        setLayoutChoiceMade(true);
+      }
+    },500);    
+  },[URLSearchParams]);
+      
 
   return (
     <main>
@@ -20,8 +53,8 @@ export default function Home() {
         <button className="SettingsButton" ></button>
       </span>
       {
-        layoutChoiceMade ? <><Avatar /><Link href="/mainPage" className="PlayButton">PLAY</Link></> 
-        : <LayoutChoice setDesktopLayout = {setDesktopLayout} setMobileLayout={setMobileLayout} setLayoutChoiceMade={setLayoutChoiceMade} />
+        layoutChoiceMade ? <><Avatar /><Link href= {{pathname:"/mainPage", query: window.location.search.toString().substring(1)}} className="PlayButton">PLAY</Link></> 
+        : <LayoutChoice setDesktopLayout={setDesktopLayout} setLayoutChoiceMade={setLayoutChoiceMade}/>
       }
     </main>
   );
--- components/LayoutChoice.tsx
+++ components/LayoutChoice.tsx
@@ -1,18 +1,46 @@
 "use client"
 import "../styles/App.css";
-import { Dispatch, JSX, SetStateAction } from "react";
+import { useEffect, useState } from "react";
+
+interface LayoutChoiceProps {
+
+  setDesktopLayout: Function;
+
+  setLayoutChoiceMade: Function;
+
+}
 
 
-export default function LayoutChoice( { setDesktopLayout, setMobileLayout, setLayoutChoiceMade }:
-     { setDesktopLayout: Dispatch<SetStateAction<boolean>>; 
-        setMobileLayout: Dispatch<SetStateAction<boolean>>; 
-        setLayoutChoiceMade: Dispatch<SetStateAction<boolean>>; }): JSX.Element {  
-return (
+export default function LayoutChoice({setDesktopLayout, setLayoutChoiceMade} : LayoutChoiceProps) {
+  let searchParams: URLSearchParams;
+  const [isLoaded,setIsLoaded] = useState(false);
+
+
+  useEffect(() => {
+    setIsLoaded(true);
+  },[]);
+
+  useEffect(() => {
+    searchParams = new URLSearchParams(window.location.search);
+  },[isLoaded]);
+
+
+  function pushLayoutinURL(layout: string):void {
+    searchParams.set("Layout", layout);
+          const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+          window.history.pushState({}, '', newUrl);
+          setDesktopLayout(true);
+          setLayoutChoiceMade(true);
+  };
+
+  // TODO then => set up styles based on layout
+
+  return (
 
     <main id="LayoutChoiceContainer">
         <h1> What device are you on ?</h1>
-        <button className="LayoutButton" onClick={() => {setDesktopLayout(true); setLayoutChoiceMade(true);}}>Desktop</button>
-        <button className="LayoutButton" onClick={() => {setMobileLayout(true); setLayoutChoiceMade(true);}}>Mobile</button>      
+        <button className="LayoutButton" onClick={() => pushLayoutinURL("Desktop")}>Desktop</button>
+        <button className="LayoutButton" onClick={() => pushLayoutinURL("Mobile")}>Mobile</button>      
     </main>
   );
 }
