baf48a106fe9d21dea2dc7d05eee1c69f86624f3
Nicolas <nicolas_312@hotmail.fr>
2025-02-07 18:12:30 +0100

    cleaning

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,12 +1,40 @@
 
+
+/*
+########################################################
+        Constants 
+########################################################
+*/
+
 export const BackgroundColorNightMode: string = "#333333";
 export const BackgroundColorLightMode: string = "#d7a7a7cc";
+export const oneDay: number = 86400000; // 1 day in milliseconds
+
+
+/*
+########################################################
+        Functions 
+########################################################
+*/
 
 // returns an array of cookies
 export function getCookies(): string[] {
-    document.cookie = "name=John Doe";
     let cookies = document.cookie.split(";").map(cookie => cookie.trim());        
 	return cookies;
+}
+
+// creates a cookie only if it does not exist
+export function createCookie(cookieName:string, cookieValue:string, expirationDate?:Date): 1 | -1 {
+    if (cookieExists(cookieName)) {
+        return -1; // error : cookie exists
+    }
+    if (!expirationDate) {
+        document.cookie = `${cookieName}=${cookieValue}}`;
+    }
+    else {
+        document.cookie = `${cookieName}=${cookieValue}; expires=${expirationDate!.toUTCString()}`;
+    }
+    return 1; // success
 }
 
 // returns true if the cookie name and value match
