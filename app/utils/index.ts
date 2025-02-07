0a45fa24f2fd39537ebc0908e255d27f1a902733
Nicolas <nicolas_312@hotmail.fr>
2025-02-07 17:50:49 +0100

    adding cookies functions

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,2 +1,33 @@
+
 export const BackgroundColorNightMode: string = "#333333";
 export const BackgroundColorLightMode: string = "#d7a7a7cc";
+
+// returns an array of cookies
+export function getCookies(): string[] {
+    document.cookie = "name=John Doe";
+    let cookies = document.cookie.split(";").map(cookie => cookie.trim());        
+	return cookies;
+}
+
+// returns true if the cookie name and value match
+export function cookieValueMatch(cookieName:string, valueToMatch:string): true | false {
+    let cookies = getCookies();
+    return cookies.indexOf(cookieName + '=' + valueToMatch) === -1 ? false : true;
+}
+
+// returns true if the cookie exists
+export function cookieExists(cookieName:string): true | false {
+    let cookies = getCookies();
+    return cookies.indexOf(cookieName) === -1 ? false : true;
+}
+
+// sets a cookie only if it exists
+export function setCookie(cookieName:string, cookieValue:string, expirationDate?:Date): void {
+    if(!cookieExists(cookieName)) {
+        return;
+    }
+    if (!expirationDate) {
+        document.cookie = `${cookieName}=${cookieValue}}`;
+    }
+    document.cookie = `${cookieName}=${cookieValue}; expires=${expirationDate!.toUTCString()}`;
+}
