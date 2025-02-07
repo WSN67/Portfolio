8fd3f85abee3b4e8232452924f77ceccd53963ad
Nicolas <nicolas_312@hotmail.fr>
2025-02-07 20:31:20 +0100

    adding different styles. Still TODO : nightmode + lightmode

--- app/mainPage/page.tsx
+++ app/mainPage/page.tsx
@@ -52,13 +52,13 @@ export default function MainPage() {
             <div id="mainPageContainer">
                 <Avatar />
                 <section>
-                    <p id="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto earum in nisi, vel perspiciatis reiciendis quibusdam suscipit sapiente consectetur, vitae officia mollitia totam? Ut, earum. Labore explicabo quia voluptas dolorem?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto earum in nisi, vel perspiciatis reiciendis quibusdam suscipit sapiente consectetur, vitae officia mollitia totam? Ut, earum. Labore explicabo quia voluptas dolorem?</p>
+                    <p id="description" className={style.description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto earum in nisi, vel perspiciatis reiciendis quibusdam suscipit sapiente consectetur, vitae officia mollitia totam? Ut, earum. Labore explicabo quia voluptas dolorem?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto earum in nisi, vel perspiciatis reiciendis quibusdam suscipit sapiente consectetur, vitae officia mollitia totam? Ut, earum. Labore explicabo quia voluptas dolorem?</p>
                     <ul id="mainPageList">
-                        <li className="listSkillItem">About me</li>
-                        <li className="listSkillItem">List of Dev Skills</li>
-                        <li className="listSkillItem">Contact</li>
-                        <li className="listSkillItem">
-                            <Link href={{pathname:"/",query: URLquery}}>Quit Game</Link>                
+                        <li className={[style.listSkillItem,"listSkillItem"].join(' ')}>About me</li>
+                        <li className={[style.listSkillItem,"listSkillItem"].join(' ')}>List of Dev Skills</li>
+                        <li className={[style.listSkillItem,"listSkillItem"].join(' ')}>Contact</li>
+                        <li className={[style.listSkillItem,"listSkillItem"].join(' ')}>
+                            <Link className={style.quitGameButton} href={{pathname:"/",query: URLquery}}>Quit Game</Link>                
                         </li>
                     </ul>
                 </section>
