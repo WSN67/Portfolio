93287a51721d4ac451518bf26379795a3cc32313
Nicolas <nicolas_312@hotmail.fr>
2025-02-10 13:50:54 +0100

    create functions for pages to cease deployments errors

---
+++ app/devSkills/page.tsx
@@ -0,0 +1,8 @@
+export default function DevSkillsPage() {
+    return (
+        <div>
+            <h1>Dev Skills page</h1>
+            <p>This is the dev skills page of the portfolio.</p>
+        </div>
+    );
+}
