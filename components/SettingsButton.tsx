47d2a28c047c3b92cc5dff9b742e1247c0e1ca56
Nicolas <nicolas_312@hotmail.fr>
2025-02-12 13:14:06 +0100

    fix nightmode button

--- components/SettingsButton.tsx
+++ components/SettingsButton.tsx
@@ -1,31 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import "../styles/SettingsButton/style.css"
 import NightMode from "../styles/SettingsButton/NightMode.module.css";
 import LightMode from "../styles/SettingsButton/LightMode.module.css";
 
-// import * as utils from "@/app/utils/index"
-
 export default function SettingsButton({ toggleLightMode, NightModeEnabled }: { toggleLightMode: () => void, NightModeEnabled: boolean } ) {
    
-    const [style,setStyle] = useState(NightMode);
-    const [isLoaded,setIsLoaded] = useState(false);
-    const [NightModeCookie,setNightModeCookie] = useState("nightmode");
-
-
-    useEffect(()=> {
-        setIsLoaded(true);
-    });
-
-
-    useEffect(() => {
-        if(isLoaded){
-            console.log(document.cookie);
-        }
-                
-    },[isLoaded,document.cookie]);
+    const [style,setStyle] = useState(NightModeEnabled ? NightMode : LightMode);
+    
+    function toggleMode() {
+        toggleLightMode();
+        style === NightMode ? setStyle(LightMode) : setStyle(NightMode);
+    }
     
     return (
-        <button onClick={toggleLightMode} className={[style.SettingsButton, "SettingsButton SettingsButtonPlace ButtonHoverEffect ButtonActiveEffect"].join(' ')} ></button>
+        <button onClick={toggleMode} className={[style.SettingsButton, "SettingsButton SettingsButtonPlace ButtonHoverEffect ButtonActiveEffect"].join(' ')} ></button>
     );
 }
