16bf40f2665d55073723275e4201c469908a5d29
Nicolas <nicolas_312@hotmail.fr>
2025-02-13 15:19:38 +0100

    ongoing work on AboutPage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+
+import SettingsButton from "@/components/SettingsButton";
+
+import NightMode from "@/styles/AboutPage/AboutNightMode.module.css"
+import LightMode from "@/styles/AboutPage/AboutLightMode.module.css"
+
 export default function AboutPage() {
+    
+    const [style,setStyle] = useState(NightMode);
+    const [NightModeEnabled,setNightModeEnabled] = useState(true);
+
+    function toggleLightMode(){
+        if (style === NightMode) {
+            setStyle(LightMode);
+            setNightModeEnabled(false);
+        }
+        else {
+            setStyle(NightMode);
+            setNightModeEnabled(true);
+
+        }
+    }
+
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>This is the about page of the portfolio.</p>
+        <div className={[style.AboutContainer, "AboutContainer"].join(' ')}>
+            <span>
+                <h1>About Page</h1>
+                <SettingsButton toggleLightMode={toggleLightMode} NightModeEnabled={NightModeEnabled}/>
+                </span>
+            <section>
+                <p>About me</p>
+            </section>
+            <section>
+                <p>About Website</p>
+                <p>Known bugs</p>
+            </section>
         </div>
     );
 }
